8b813bbaeae5cc7d5722d73a28013ecfa036be1e
Jinseong Hwang <jinseong.dev@gmail.com>
2022-07-12 23:02:27 +0900

    feat: chapter 03-1

---
+++ chapter03/src/main/java/me/study/unittest/Calculator.java
@@ -0,0 +1,8 @@
+package me.study.unittest;
+
+public class Calculator {
+
+    public double sum(double first, double second) {
+        return first + second;
+    }
+}
---
+++ chapter03/src/test/java/me/study/unittest/Example3_1.java
@@ -0,0 +1,22 @@
+package me.study.unittest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Example3_1 {
+
+    @Test
+    void Sum_of_two_numbers() throws Exception {
+        // Given
+        double first = 10;
+        double second = 20;
+        final Calculator calculator = new Calculator();
+
+        // When
+        final double result = calculator.sum(first, second);
+
+        // Then
+        assertEquals(30, result);
+    }
+}
